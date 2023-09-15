b7eece1491eb4cc8335321f10aa5ff6f6c507b1c
ARJUN-TUDU <64701428+ARJUN-TUDU@users.noreply.github.com>
2023-09-15 11:38:44 +0530

    path added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors")
 const dotenv = require("dotenv");
 const app = express();
+
 app.use(cors(
     {
         origin: ["http://localhost:3000/"],
@@ -11,9 +12,7 @@ app.use(cors(
     }
 ));
 app.use(express.json());
-
 dotenv.config();
-
 const MONGODB_URI = process.env.MONGODB_URI
 
 
@@ -23,6 +22,18 @@ try {
     console.log("mongoose connection error")
 }
 
+
+if (process.env.NODE_ENV === "production") {
+    const path = require("path");
+    app.use(express.static(path.resolve(__dirname, 'frontend', 'build')));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'),function (err) {
+            if(err) {
+                res.status(500).send(err)
+            }
+        });
+    })
+}
 
 const User = mongoose.model("collection_1",{
    
