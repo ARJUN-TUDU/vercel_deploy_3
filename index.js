8f6a4461d4c755ab0817e4145bc6882bb05019b0
ARJUN-TUDU <64701428+ARJUN-TUDU@users.noreply.github.com>
2023-09-17 23:50:29 +0530

    commited

--- index.js
+++ index.js
@@ -1,63 +1,50 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors")
-const dotenv = require("dotenv");
 const app = express();
-const bodyParser = require('body-parser');
-const { urlencoded, json } = require('body-parser');
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-dotenv.config();
 
+try{
 
+    mongoose.connect
+    ('mongodb+srv://arjuntudu:AnhuN5IU1fO37iWL@cluster0.0ttkmlw.mongodb.net/?retryWrites=true&w=majority');
+    console.log("db connection established")
 
-try {
-    mongoose.connect('mongodb+srv://arjuntudu:Redhawse*1@cluster0.0ttkmlw.mongodb.net/?retryWrites=true&w=majority/db_1')
-}catch(err){
-    console.log("mongoose connection error")
+}catch(e){
+
+    if(e){
+        console.log("db connection failed");
+    }
 }
 
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json());
 
 const User = mongoose.model("collection_1",{
-   
     name : String,
     age : Number
-
 })
-
-
-
 
 app.get("/",async (req,res)=>{
-   
-     try {
 
-        const data =  await User.find();
+
+    try {
+        const data = await User.find();
         res.json(data);
-
-  
-     }catch(e){
-
-        console.log(" getting data error ")
-        
-     }
-
+    }catch(err){
+        if(err){
+            console.log("app get failed")
+        }
+    }
+    
 })
 
-app.get('*',(req,res,next)=>{
-    res.status(200).json({
-      message:'succeess'
-    })
-  })
-
-
-app.listen(9000,(err)=>{
+app.listen(PORT,(err)=>{
 
     if(err){
-        console.log("error starting")
+        console.log("app started failed")
     }else{
         console.log("app started")
     }
