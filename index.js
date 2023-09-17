4cc617170a77ecc58ae9e4f6a284edd35dafb0be
ARJUN-TUDU <64701428+ARJUN-TUDU@users.noreply.github.com>
2023-09-17 23:58:59 +0530

    commited

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
+dotenv.config()
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors(
+    {
+        origin: ["https://verce-l-frontend.vercel.app"],
+        methods: ["POST", "GET"],
+        credentials: true
+    }
+));
 app.use(express.json());
 
 try{
